0fb16294b2056eeccd6c9424d25492392a5c437b
glebychyasher <zobningleb01@icloud.com>
2023-12-18 20:30:52 +0300

    Initial Commit

---
+++ DataFlow4.4/DataFlow4_4App.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlow4_4App.swift
+//  DataFlow4.4
+//
+//  Created by Глеб Зобнин on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct DataFlow4_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
